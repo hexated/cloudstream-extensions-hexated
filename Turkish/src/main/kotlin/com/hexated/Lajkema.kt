367e40d19e4a5bd3922ad80bc4cdb12c37b29eb6
hexated <hexated@gmail.com>
2022-09-26 05:48:43 +0700

    added Turkish123

---
+++ Turkish/src/main/kotlin/com/hexated/Lajkema.kt
@@ -0,0 +1,8 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.extractors.XStreamCdn
+
+class Lajkema : XStreamCdn() {
+    override val name: String = "Lajkema"
+    override val mainUrl: String = "https://lajkema.com"
+}
