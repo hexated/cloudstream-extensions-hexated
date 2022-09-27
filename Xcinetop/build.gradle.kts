851e14c20abdb6d74d65db86603ec2cd54107f00
hexated <hexated@gmail.com>
2022-09-28 00:06:07 +0700

    fixed added Xcinetop icon

--- Xcinetop/build.gradle.kts
+++ Xcinetop/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
         "Movie",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=xcine.top&sz=%size%"
+    iconUrl = "https://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/master/Xcinetop/icon.png"
 }
