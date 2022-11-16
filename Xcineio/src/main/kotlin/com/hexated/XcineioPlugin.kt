2eebdfb920ad5df8f5ecb1e0063b924ed8c2f1ae
hexated <hexated@gmail.com>
2022-11-16 22:37:58 +0700

    added Xcineio

---
+++ Xcineio/src/main/kotlin/com/hexated/XcineioPlugin.kt
@@ -0,0 +1,20 @@
+
+package com.hexated
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class XcineioPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(Xcineio())
+        registerExtractorAPI(StreamTapeAdblockuser())
+        registerExtractorAPI(StreamTapeTo())
+        registerExtractorAPI(Mixdrp())
+        registerExtractorAPI(DoodReExtractor())
+        registerExtractorAPI(Streamzz())
+        registerExtractorAPI(Streamcrypt())
+    }
+}
