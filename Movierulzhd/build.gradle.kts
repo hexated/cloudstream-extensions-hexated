6705963d33ad6d7700f27383f10f8811860685b3
hexated <hexated@gmail.com>
2022-12-02 17:24:42 +0700

    update domain Movierulzhd

--- Movierulzhd/build.gradle.kts
+++ Movierulzhd/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 23
+version = 24
 
 
 cloudstream {
--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class Movierulzhd : MainAPI() {
-    override var mainUrl = "https://movierulzhd.life"
+    override var mainUrl = "https://movierulzhd.pics"
     private var directUrl = mainUrl
     override var name = "Movierulzhd"
     override val hasMainPage = true
