f85a021b5c9ef85ac7278d31fc31550e55050f37
hexated <hexated@gmail.com>
2022-12-13 21:14:28 +0700

    fixed Movierulzhd

--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class Movierulzhd : MainAPI() {
-    override var mainUrl = "https://movierulzhd.pics"
+    override var mainUrl = "https://movierulzhd.cfd"
     private var directUrl = mainUrl
     override var name = "Movierulzhd"
     override val hasMainPage = true
