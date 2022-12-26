6bba06e45b43bfa3a02186625499d83d68aae132
hexated <hexated@gmail.com>
2022-12-27 04:29:56 +0700

    it's better have than nothing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 
-val disabled = listOf<String>("Animixplay")
+val disabled = listOf<String>()
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
