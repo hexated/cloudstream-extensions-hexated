01cb7183b6976e9fe99af6458bc20aa597460545
Pavan Katari <43464081+pavankatari@users.noreply.github.com>
2023-01-15 10:01:44 +0530

    Update New Domain

--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class Movierulzhd : MainAPI() {
-    override var mainUrl = "https://movierulzhd.shop"
+    override var mainUrl = "https://movierulzhd.store"
     private var directUrl = mainUrl
     override var name = "Movierulzhd"
     override val hasMainPage = true
