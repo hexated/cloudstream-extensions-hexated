285eb25bc8e4c92f90ff22e121bcc9db1edab8df
hexated <hexated@gmail.com>
2023-01-17 10:55:16 +0700

    [Sora] added 123Movies

--- SoraStream/src/main/kotlin/com/hexated/SoraStreamPlugin.kt
+++ SoraStream/src/main/kotlin/com/hexated/SoraStreamPlugin.kt
@@ -11,5 +11,6 @@ class SoraStreamPlugin: Plugin() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(SoraStream())
         registerExtractorAPI(StreamM4u())
+        registerExtractorAPI(Sblongvu())
     }
 }
