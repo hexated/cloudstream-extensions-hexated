4edcde11b022e167736a67cf6723f7d91918b894
hexated <hexated@gmail.com>
2023-01-17 18:11:51 +0700

    added new sources into Kuramanime & Oploverz

--- KuramanimeProvider/src/main/kotlin/com/hexated/Nyomo.kt
+++ KuramanimeProvider/src/main/kotlin/com/hexated/Nyomo.kt
@@ -3,5 +3,11 @@ package com.hexated
 import com.lagradost.cloudstream3.extractors.StreamSB
 
 class Nyomo : StreamSB() {
+    override var name: String = "Nyomo"
     override var mainUrl = "https://nyomo.my.id"
+}
+
+class Streamhide : StreamSB() {
+    override var name: String = "Streamhide"
+    override var mainUrl: String = "https://streamhide.to"
 }
