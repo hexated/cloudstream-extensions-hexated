ff143eedd46739e3eef4aa7a33842ac6d530e958
tuan041 <30403510+tuan041@users.noreply.github.com>
2023-01-29 12:42:20 +0700

    Update PhimmoichillProvider.kt

--- PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
+++ PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 import java.net.URLDecoder
 
 class PhimmoichillProvider : MainAPI() {
-    override var mainUrl = "https://phimmoichilla.net/"
+    override var mainUrl = "https://phimmoichilla.net"
     override var name = "Phimmoichill"
     override val hasMainPage = true
     override var lang = "vi"
