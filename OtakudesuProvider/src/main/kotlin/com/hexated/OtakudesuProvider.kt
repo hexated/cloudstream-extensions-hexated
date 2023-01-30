923236dd40ca44ef1a0e7513ed24b4a0003a911d
hexated <hexated@gmail.com>
2023-01-30 22:08:25 +0700

    [Otakudesu] changed domain

--- OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
+++ OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
@@ -13,7 +13,7 @@ import org.jsoup.nodes.Element
 import java.util.ArrayList
 
 class OtakudesuProvider : MainAPI() {
-    override var mainUrl = "https://otakudesu.is"
+    override var mainUrl = "https://otakudesu.asia"
     override var name = "Otakudesu"
     override val hasMainPage = true
     override var lang = "id"
