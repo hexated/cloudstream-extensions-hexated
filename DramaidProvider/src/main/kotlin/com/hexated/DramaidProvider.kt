fdd244ed881ba312ef735cf4aa55a2594884bd1a
hexated <hexated@gmail.com>
2023-02-08 21:55:54 +0700

    Dramaid: updated domain

--- DramaidProvider/src/main/kotlin/com/hexated/DramaidProvider.kt
+++ DramaidProvider/src/main/kotlin/com/hexated/DramaidProvider.kt
@@ -11,7 +11,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class DramaidProvider : MainAPI() {
-    override var mainUrl = "https://dramaid.cloud"
+    override var mainUrl = "hhttps://dramaid.best"
     override var name = "DramaId"
     override val hasQuickSearch = false
     override val hasMainPage = true
--- Kickassanime/build.gradle.kts
+++ Kickassanime/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 10
+version = 9
 
 
 cloudstream {
