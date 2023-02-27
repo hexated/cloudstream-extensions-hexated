07f3a57857b906d1eff12bd90a0190adcaa30c6a
hexated <hexated@gmail.com>
2023-02-27 16:49:35 +0700

    AnimeIndo: added new source

--- AnimeIndoProvider/src/main/kotlin/com/hexated/AnimeIndoProviderPlugin.kt
+++ AnimeIndoProvider/src/main/kotlin/com/hexated/AnimeIndoProviderPlugin.kt
@@ -11,5 +11,6 @@ class AnimeIndoProviderPlugin: Plugin() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(AnimeIndoProvider())
         registerExtractorAPI(Vicloud())
+        registerExtractorAPI(Uservideo())
     }
 }
