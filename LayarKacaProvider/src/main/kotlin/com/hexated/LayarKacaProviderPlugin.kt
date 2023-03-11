4e9e42da2a2505807da7eb009295dec5e4998fc0
hexated <hexated@gmail.com>
2023-03-12 02:20:52 +0700

    LayarKaca: updated domain

--- LayarKacaProvider/src/main/kotlin/com/hexated/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/hexated/LayarKacaProviderPlugin.kt
@@ -10,6 +10,5 @@ class LayarKacaProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(LayarKacaProvider())
-        registerExtractorAPI(Filemoon())
     }
 }
