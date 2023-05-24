c4c28d54b494accecc49639317c9282bff64c72b
givemyusd <134507513+givemyusd@users.noreply.github.com>
2023-05-24 22:03:57 +0530

    Update domain url

--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class Movierulzhd : MainAPI() {
-    override var mainUrl = "https://movierulzhd.press"
+    override var mainUrl = "https://movierulzhd.wiki"
     private var directUrl = mainUrl
     override var name = "Movierulzhd"
     override val hasMainPage = true
