b2cc9c4625af73001c8be6b0311ed79a417dc5e6
Hexated <37908684+hexated@users.noreply.github.com>
2023-06-11 10:48:04 +0700

    Update KuronimeProvider.kt

--- KuronimeProvider/src/main/kotlin/com/hexated/KuronimeProvider.kt
+++ KuronimeProvider/src/main/kotlin/com/hexated/KuronimeProvider.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 import java.util.ArrayList
 
 class KuronimeProvider : MainAPI() {
-    override var mainUrl = "https://45.12.2.25"
+    override var mainUrl = "https://kuronime.lol"
     override var name = "Kuronime"
     override val hasQuickSearch = true
     override val hasMainPage = true
