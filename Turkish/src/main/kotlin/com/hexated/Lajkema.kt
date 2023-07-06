54f74509a8d25f3e9a96aeb57f63d32c777cbbb0
KingLucius <escuro.anjo@gmail.com>
2023-07-06 14:50:25 +0300

    Just Keep SoraStream

--- Turkish/src/main/kotlin/com/hexated/Lajkema.kt
+++
@@ -1,8 +0,0 @@
-package com.hexated
-
-import com.lagradost.cloudstream3.extractors.XStreamCdn
-
-class Lajkema : XStreamCdn() {
-    override val name: String = "Lajkema"
-    override val mainUrl: String = "https://lajkema.com"
-}
