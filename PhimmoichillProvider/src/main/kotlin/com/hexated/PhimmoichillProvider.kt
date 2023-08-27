9b2a405cf82c6696a8604c24693e3fdaf40762af
movieforme <142312645+movieforme@users.noreply.github.com>
2023-08-27 17:35:41 +0700

    fix domain

--- PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
+++ PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 import java.net.URLDecoder
 
 class PhimmoichillProvider : MainAPI() {
-    override var mainUrl = "https://phimmoichillg.net"
+    override var mainUrl = "https://phimmoichilld.net"
     override var name = "Phimmoichill"
     override val hasMainPage = true
     override var lang = "vi"
