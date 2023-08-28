cca05ec55928f0a5659782abda7204656a3e7ce2
Son Goku <39613333+gemboxhost@users.noreply.github.com>
2023-08-28 15:29:50 +0800

    Update IdlixProvider.kt

--- IdlixProvider/src/main/kotlin/com/hexated/IdlixProvider.kt
+++ IdlixProvider/src/main/kotlin/com/hexated/IdlixProvider.kt
@@ -13,7 +13,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class IdlixProvider : MainAPI() {
-    override var mainUrl = "https://tv.idlixprime.com"
+    override var mainUrl = "https://tv.idlixplus.net"
     private var directUrl = mainUrl
     override var name = "Idlix"
     override val hasMainPage = true
