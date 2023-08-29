54dc8ce2c915b6e0ee990edddf3e709045001df2
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-08-29 19:02:42 +0530

    update: hdmovie2 and movierulz domain (#250)
    * Update Movierulzhd.kt
    
    * Update Hdmovie2.kt

--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -11,7 +11,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 open class Movierulzhd : MainAPI() {
-    override var mainUrl = "https://movierulzhd.gold"
+    override var mainUrl = "https://movierulzhd.tools"
     var directUrl = ""
     override var name = "Movierulzhd"
     override val hasMainPage = true
