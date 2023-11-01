3a306d807592a15597cdb34058cf7ba8eadc2672
jack <jack@gmail.com>
2023-11-02 02:40:43 +0700

    kuronime: update domain

--- KuronimeProvider/src/main/kotlin/com/hexated/KuronimeProvider.kt
+++ KuronimeProvider/src/main/kotlin/com/hexated/KuronimeProvider.kt
@@ -16,7 +16,7 @@ import java.net.URI
 import java.util.ArrayList
 
 class KuronimeProvider : MainAPI() {
-    override var mainUrl = "https://45.12.2.28"
+    override var mainUrl = "https://tv.kuronime.vip"
     private var animekuUrl = "https://animeku.org"
     override var name = "Kuronime"
     override val hasQuickSearch = true
