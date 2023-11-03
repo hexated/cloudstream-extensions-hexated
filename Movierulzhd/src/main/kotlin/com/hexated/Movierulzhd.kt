bb641ac1af19603cafba5d5cb0353e1dcf1982c8
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-11-03 14:13:14 +0530

    Update Movierulzhd.kt

--- Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Movierulzhd.kt
@@ -12,7 +12,7 @@ import java.net.URI
 
 open class Movierulzhd : MainAPI() {
 
-    override var mainUrl = "https://movierulzhd.wine"
+    override var mainUrl = "https://movierulzhd.show"
 
     var directUrl = ""
     override var name = "Movierulzhd"
