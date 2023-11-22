f04d02408533b8c867fb27fafb4c42ef9f0d6b1f
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-11-22 15:55:08 +0530

    Update build.gradle.kts (#399)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.4")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
 
@@ -42,12 +42,10 @@ subprojects {
     }
 
     android {
-        compileSdkVersion(33)
-
         defaultConfig {
             minSdk = 21
+            compileSdkVersion(33)
             targetSdk = 33
-
         }
 
         compileOptions {
@@ -77,14 +75,14 @@ subprojects {
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
+
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
         implementation("com.github.Blatzar:NiceHttp:0.4.4") // http library
-        implementation("org.jsoup:jsoup:1.16.1") // html parser
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("org.jsoup:jsoup:1.16.2") // html parser
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
         implementation("io.karn:khttp-android:0.1.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // html parser
-        //run JS
-        implementation("org.mozilla:rhino:1.7.14")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // html parser
+        implementation("org.mozilla:rhino:1.7.14") //run JS
 
     }
 }
