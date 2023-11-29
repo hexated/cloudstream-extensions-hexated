ce5ecba288b1d0ba4c825168b30da385735263f4
adityajd <45625055+adityajd@users.noreply.github.com>
2023-11-29 07:04:46 +0700

    Update Cgvindo.kt
    update domain

--- RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
+++ RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
@@ -3,7 +3,7 @@ package com.hexated
 import com.lagradost.cloudstream3.TvType
 
 class Cgvindo : RebahinProvider() {
-    override var mainUrl = "https://cgvindo.com/amp/"
+    override var mainUrl = "http://167.86.79.231"
     override var name = "Cgvindo"
 
 }
