1705e34a3cb04fb152927514d763c1e4263c7f73
irfannajmudinsidik <50092754+irfannajmudinsidik@users.noreply.github.com>
2023-12-23 22:15:23 +0700

    Update Cgvindo.kt

--- RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
+++ RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
@@ -3,7 +3,7 @@ package com.hexated
 import com.lagradost.cloudstream3.TvType
 
 class Cgvindo : RebahinProvider() {
-    override var mainUrl = "http://167.86.79.231"
+    override var mainUrl = "http://198.54.124.245"
     override var name = "Cgvindo"
 
 }
