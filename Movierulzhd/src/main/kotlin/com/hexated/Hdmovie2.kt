f30b0039498c217f5a8da7bad13c734e8325a825
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-12-28 15:58:35 +0530

    Update Hdmovie2.kt

--- Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
@@ -12,7 +12,7 @@ import org.jsoup.Jsoup
 
 class Hdmovie2 : Movierulzhd() {
 
-    override var mainUrl = "https://hdmovie2.li"
+    override var mainUrl = "https://hdmovie2.foo"
     override var name = "Hdmovie2"
     override val mainPage = mainPageOf(
         "trending" to "Trending",
