aca1625a12a10b3cec513a8f698f2fc6fdcb1c2f
alex <alex@users.noreply.github.com>
2024-01-06 15:14:01 +0700

    update

--- SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
+++ SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
@@ -257,7 +257,7 @@ open class SoraStream : TmdbProvider() {
                 this.backgroundPosterUrl = bgPoster
                 this.year = year
                 this.plot = res.overview
-                this.tags = if (isAnime) keywords else genres
+                this.tags = keywords
                 this.rating = rating
                 this.showStatus = getStatus(res.status)
                 this.recommendations = recommendations
@@ -281,7 +281,7 @@ open class SoraStream : TmdbProvider() {
                 this.year = year
                 this.plot = res.overview
                 this.duration = res.runtime
-                this.tags = if (isAnime) keywords else genres
+                this.tags = keywords
                 this.rating = rating
                 this.recommendations = recommendations
                 this.actors = actors
