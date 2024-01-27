c451168f1bcbedf0aa64319ced8859d904f1a927
Nexus <79303560+Nexus-Gits@users.noreply.github.com>
2024-01-27 20:05:18 +0530

    Create build.gradle.kts

---
+++ UncutMaza/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("hexated")
+    language    = "hi"
+    description = "We Provide Huge Colocation of Hindi Porn (Uncut) Web Series"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.com&sz=%size%"
+}
