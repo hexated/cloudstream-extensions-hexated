170b8abc2e622867dd0e2947097998ca814fd1b4
Nexus <79303560+Nexus-Gits@users.noreply.github.com>
2024-01-27 20:20:54 +0530

    Update build.gradle.kts

--- UncutMaza/build.gradle.kts
+++ UncutMaza/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.cc&sz=%size%"
 }
