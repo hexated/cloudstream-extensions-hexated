ff78487f88c9aec9db5d4098bd7686348933732c
IndusAryan <serena01011975@gmail.com>
2024-02-24 15:32:13 +0530

    update domains 💀

--- Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
@@ -12,7 +12,7 @@ import org.jsoup.Jsoup
 
 class Hdmovie2 : Movierulzhd() {
 
-    override var mainUrl = "https://hdmovie2.mba"
+    override var mainUrl = "https://hdmovie2.tax"
     override var name = "Hdmovie2"
     override val mainPage = mainPageOf(
         "trending" to "Trending",
--- SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
+++ SoraStream/src/main/kotlin/com/hexated/SoraStream.kt
@@ -118,12 +118,11 @@ open class SoraStream : TmdbProvider() {
         const val moviefictionAPI = "https://moviefiction.com"
         const val zoechipAPI = "https://zoechip.org"
         const val nepuAPI = "https://nepu.to"
-
         const val fdMoviesAPI = "https://freedrivemovie.com"
-        const val uhdmoviesAPI = "https://uhdmovies.us"
+        const val uhdmoviesAPI = "https://uhdmovies.asia"
         const val hdmovies4uAPI = "https://hdmovies4u.day"
-        const val vegaMoviesAPI = "https://vegamovies.ngo"
-        const val dotmoviesAPI = "https://dotmovies.dad"
+        const val vegaMoviesAPI = "https://vegamovies.ong"
+        const val dotmoviesAPI = "https://dotmovies.one"
         const val tvMoviesAPI = "https://www.tvseriesnmovies.com"
         const val dahmerMoviesAPI = "https://odd-bird-1319.zwuhygoaqe.workers.dev"
 
