bd3047b7be937226b348f7a8a3bf0e321712e3ef
IndusAryan <serena01011975@gmail.com>
2024-02-24 16:10:45 +0530

    fix workflows and update nicehttp

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,11 +77,11 @@ subprojects {
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
 
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.4.5") // http library
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
         implementation("org.jsoup:jsoup:1.17.2") // html parser
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
         implementation("io.karn:khttp-android:0.1.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // html parser
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
         implementation("org.mozilla:rhino:1.7.14") //run JS
 
     }
