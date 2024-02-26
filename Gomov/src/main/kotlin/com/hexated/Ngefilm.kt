00e9fe28618251d531b8c2cf27a09f860efab72f
adityajd <45625055+adityajd@users.noreply.github.com>
2024-02-26 21:06:58 +0700

    Update Ngefilm.kt
    Update mainurl

--- Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
+++ Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
@@ -3,7 +3,7 @@ package com.hexated
 import com.lagradost.cloudstream3.mainPageOf
 
 class Ngefilm : Gomov() {
-    override var mainUrl = "https://tv.ngefilm21.homes"
+    override var mainUrl = "https://ngefilm21.pics"
     override var name = "Ngefilm"
     override val mainPage = mainPageOf(
         "/page/%d/?s&search=advanced&post_type=movie&index&orderby&genre&movieyear&country&quality=" to "Movies Terbaru",
